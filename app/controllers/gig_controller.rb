04657adfcc0cb16d96a08f91804ae1ebd9a1b40b
RespectedCow <secretivemoo@gmail.com>
2021-09-06 16:14:59 +0800

    Landing page

---
+++ app/controllers/gig_controller.rb
@@ -0,0 +1,6 @@
+class GigController < ApplicationController
+    def create 
+
+    end 
+    
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in? == false
+      redirect_to landing_path
+    end
   end
   
   def landing
